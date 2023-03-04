dc35f498dc8825edfd5170c085c01cbea5daec34
Justin Luangrath <justinluangrath@gmail.com>
2023-03-04 11:31:03 -0800

    added video player component and styling

---
+++ src/components/VideoPlayer/VideoPlayer.js
@@ -0,0 +1,13 @@
+import "./VideoPlayer.scss";
+
+const VideoPlayer = (props) => {
+  let { currentVideoId, videoList } = props;
+  let video = videoList.find((video) => video.id === currentVideoId);
+  return (
+    <div className="player__container">
+      <video className="player" controls poster={video.image}></video>
+    </div>
+  );
+};
+
+export default VideoPlayer;
