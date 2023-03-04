216005254a3fb745e3beac2db0c9ca2eecd896d5
Justin Luangrath <justinluangrath@gmail.com>
2023-03-04 14:50:29 -0800

    added comment section component and styling

---
+++ src/components/CommentSection/CommentSection.js
@@ -0,0 +1,20 @@
+import "./CommentSection.scss";
+import CommentForm from "../CommentForm/CommentForm";
+import Comment from "../Comment/Comment";
+import { v4 as uuidv4 } from "uuid";
+
+const CommentSection = (props) => {
+  let { currentVideoId, videoDetails } = props;
+  let currentVideo = videoDetails.find((video) => video.id === currentVideoId);
+  return (
+    <div className="comment-section">
+      <h4>{currentVideo.comments.length + " Comments"}</h4>
+      <CommentForm />
+      {currentVideo.comments?.map((comment, index) => (
+        <Comment key={uuidv4()} video={comment} />
+      ))}
+    </div>
+  );
+};
+
+export default CommentSection;
